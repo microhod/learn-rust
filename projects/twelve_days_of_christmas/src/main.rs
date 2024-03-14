ea19a6725c9e1655f3c5645d8587f02c4c314264
Sam Hodgkinson <55709069+microhod@users.noreply.github.com>
2024-03-14 18:00:39 +0000

    add 12 days of christmas

---
+++ projects/twelve_days_of_christmas/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    for day in 1..=12 {
+        println!();
+        print_verse_for_day(day);
+    }
+}
+
+fn print_verse_for_day(day: usize) {
+    let nth = match day {
+        1 => "st",
+        2 => "nd",
+        3 => "rd",
+        _ => "th",
+    };
+    println!("On the {day}{nth} day of Christmas");
+    println!("my true love gave to me");
+
+    let presents = [
+        "Twelve drummers drumming",
+        "Eleven pipers piping",
+        "Ten lords a-leaping",
+        "Nine ladies dancing",
+        "Eight maids a-milking",
+        "Seven swans a-swimming",
+        "Six geese a-laying",
+        "Five golden rings",
+        "Four calling birds",
+        "Three French hens",
+        "Two turtle doves",
+        "partridge in a pear tree!"
+    ];
+
+    let start: usize = 11 - (day - 1);
+    for i in start..11 {
+        println!("{}", presents[i])
+    }
+
+    if day > 1 {
+        println!("And a partridge in a pear tree!");
+        return;
+    }
+    println!("A partridge in a pear tree!");
+}
